cbbe35a5627e8d98cbb7c6d7c7e9d7b7d4039838
iso4096 <45616468+iso4096@users.noreply.github.com>
2021-05-23 16:25:22 +0800

    tesdt2

--- sketch.js
+++ sketch.js
@@ -1,20 +1,24 @@
 let img;
+var theta = 0;
 
 function preload() {
   img = loadImage('assets/zh.jpg');
 }
 
 function setup() {
-  createCanvas(100, 100, WEBGL);
+    createCanvas(windowWidth, windowHeight, WEBGL)
 }
-
+  
 function draw() {
-  texture(img);
-  textureMode(NORMAL);
-  beginShape();
-  vertex(-20, -40, 0, 0);
-  vertex(20, -50, 1, 0);
-  vertex(20, 20, 1, 1);
-  vertex(-40, 20, 0, 1);
-  endShape();
-}
+    background(255, 255, 255, 255);
+  
+    rotateZ(theta * mouseX * 0.001);
+    rotateX(theta * mouseX * 0.001);
+    rotateY(theta * mouseX * 0.001);
+  
+    //pass image as texture
+    texture(img);
+    sphere(80);
+  
+    theta += 0.05;
+  }
