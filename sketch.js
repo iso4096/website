32d7b24e20c5c62610825fcfe935307d5652e8aa
iso4096 <45616468+iso4096@users.noreply.github.com>
2021-05-23 16:38:03 +0800

    zoom test

--- sketch.js
+++ sketch.js
@@ -13,13 +13,13 @@ function draw() {
     background(255, 255, 255, 255);
     noStroke();
   
-    rotateZ(theta * 0.000005);
-    rotateX(theta * 0.000005);
-    rotateY(theta * 0.000005);
+    rotateZ(0.001);
+    rotateX(0.001);
+    rotateY(0.001);
   
     //pass image as texture
     texture(img);
     sphere(300, 100, 100);
-  
-    theta ++;
+
+    theta += theta > 100 ? 0 : 0.01;
   }
